d4370a2aa22f327d15a5d567624d3de59ffc795d
DmitryK777 <kislyakovdmv@gmail.com>
2024-04-21 15:54:56 +0300

    IntroductionToOOP

--- IntroductionToOOP/main.cpp
+++ IntroductionToOOP/main.cpp
@@ -1,8 +1,59 @@
 #include<iostream>
 using namespace std;
 
+class Point       // Создаём структуру 'Point', которая будет описывать точки на плоскости
+{
+
+	double x;
+	double y;
+
+public:
+	double get_x()const
+	{
+		return x;
+	}
+
+	double get_y()const
+	{
+		return y;
+	}
+
+	void set_x(double x)
+	{
+		this->x = x;
+	}
+
+	void set_y(double y)
+	{
+		this->y = y;
+	}
+};
+
+//#define STRUCT_POINT
 void main()
 {
 	setlocale(LC_ALL, "");
+#if defined STRUCT_POINT
 	cout << "Hello OOP" << endl;
+
+	int a;            // Объявление переменной 'a' типа int
+	Point A;          // Объявление переменной 'A' типа Point
+	                  // Создание объекта 'A' структуры 'Point'
+					  // Создание экземпляра структуры 'Point'
+
+	A.x = 2;
+	A.y = 3;
+	cout << A.x << "\t" << A.y << endl;
+	cout << sizeof(Point) << endl;
+	cout << sizeof(A) << endl;
+
+	Point* pA = &A;
+	cout << (*pA).x << "\t" << (*pA).y << endl;
+	cout << pA->x << "\t" << pA->y << endl;
+#endif
+
+	Point A;
+	A.set_x(2);
+	A.set_y(3);
+	cout << A.get_x() << "\t" << A.get_y() << endl;
 }
