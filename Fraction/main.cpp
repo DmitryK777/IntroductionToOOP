7cc082b960f13fddc4664af3bab8c0ad5e1f3d16
DmitryK777 <kislyakovdmv@gmail.com>
2024-05-17 16:30:46 +0300

    home work is done

--- Fraction/main.cpp
+++ Fraction/main.cpp
@@ -2,8 +2,15 @@
 #include<iostream>
 using namespace std;
 
+using std::cin;
+using std::cout;
+using std::endl;
+
+
+
 class Fraction
 {
+private:
 	int integer;
 	int numerator;
 	int denominator;
@@ -77,6 +84,191 @@ public:
 		return *this;
 	}
 
+	Fraction& operator++() // Preffix increment
+	{
+		++this->integer;
+		return *this;
+	}
+
+	Fraction& operator++(int) // Suffix increment
+	{
+		Fraction old = *this;
+
+		this->integer++;
+
+		return old;
+	}
+
+	Fraction& operator--() // Preffix increment
+	{
+		if (this->integer == 0 && this->numerator > 0) this->numerator -= this->denominator;
+		else --this->integer;
+		if (this->integer < 0 && this->numerator < 0) this->numerator *= (-1);
+		return *this;
+	}
+
+	Fraction& operator--(int) // Suffix increment
+	{
+		Fraction old = *this;
+
+		if (this->integer == 0 && this->numerator > 0) this->numerator -= this->denominator;
+		else this->integer--;
+		if (this->integer < 0 && this->numerator < 0) this->numerator *= (-1);
+		return old;
+	}
+
+	Fraction& operator+=(const Fraction& other)
+	{
+		int denominator = 1;
+		int numerator = 0;
+		int integer = 0;
+
+		int numerator_left = 0;
+		int numerator_right = 0;
+
+		if (this->get_integer() >= 0) numerator_left = (this->get_integer() * this->get_denominator() + this->get_numerator());
+		else numerator_left = (this->get_integer() * this->get_denominator() - this->get_numerator());
+		if (other.get_integer() >= 0) numerator_right = (other.get_integer() * other.get_denominator() + other.get_numerator());
+		else numerator_right = (other.get_integer() * other.get_denominator() - other.get_numerator());
+		
+		denominator = this->get_denominator() * other.get_denominator();
+		
+		numerator_left *= other.get_denominator();
+		numerator_right *= this->get_denominator();
+		numerator = numerator_left + numerator_right;
+
+		integer = numerator / denominator;
+		numerator %= denominator;
+		if (numerator < 0) numerator *= (-1);
+
+		int max_comon_divider = 1;
+		for (int i = 1; i <= numerator; i++)
+		{
+			if (numerator % i == 0 && denominator % i == 0) max_comon_divider = i;
+		}
+
+		numerator /= max_comon_divider;
+		denominator /= max_comon_divider;
+
+		this->set_integer(integer);
+		this->set_numerator(numerator);
+		this->set_denominator(denominator);
+
+		return *this;
+	}
+
+	Fraction& operator-=(const Fraction& other)
+	{
+		int denominator = 1;
+		int numerator = 0;
+		int integer = 0;
+
+		int numerator_left = 0;
+		int numerator_right = 0;
+
+		if (this->get_integer() >= 0) numerator_left = (this->get_integer() * this->get_denominator() + this->get_numerator());
+		else numerator_left = (this->get_integer() * this->get_denominator() - this->get_numerator());
+		if (other.get_integer() >= 0) numerator_right = (other.get_integer() * other.get_denominator() + other.get_numerator());
+		else numerator_right = (other.get_integer() * other.get_denominator() - other.get_numerator());
+		denominator = this->get_denominator() * other.get_denominator();
+		numerator_left *= other.get_denominator();
+		numerator_right *= this->get_denominator();
+
+		numerator = numerator_left - numerator_right;
+		integer = numerator / denominator;
+		numerator %= denominator;
+		if (numerator < 0) numerator *= (-1);
+
+		int max_comon_divider = 1;
+		for (int i = 1; i <= numerator; i++)
+		{
+			if (numerator % i == 0 && denominator % i == 0) max_comon_divider = i;
+		}
+
+		numerator /= max_comon_divider;
+		denominator /= max_comon_divider;
+
+		this->set_integer(integer);
+		this->set_numerator(numerator);
+		this->set_denominator(denominator);
+
+		return *this;
+	}
+
+	Fraction& operator*=(const Fraction& other)
+	{
+		int denominator = 1;
+		int numerator = 0;
+		int integer = 0;
+
+		int numerator_left = 0;
+		int numerator_right = 0;
+
+		if (this->get_integer() >= 0) numerator_left = (this->get_integer() * this->get_denominator() + this->get_numerator());
+		else numerator_left = (this->get_integer() * this->get_denominator() - this->get_numerator());
+		if (other.get_integer() >= 0) numerator_right = (other.get_integer() * other.get_denominator() + other.get_numerator());
+		else numerator_right = (other.get_integer() * other.get_denominator() - other.get_numerator());
+
+		numerator = numerator_left * numerator_right;
+		denominator = this->get_denominator() * other.get_denominator();
+		integer = numerator / denominator;
+		numerator %= denominator;
+		if (numerator < 0) numerator *= (-1);
+
+		int max_comon_divider = 1;
+		for (int i = 1; i <= numerator; i++)
+		{
+			if (numerator % i == 0 && denominator % i == 0) max_comon_divider = i;
+		}
+
+		numerator /= max_comon_divider;
+		denominator /= max_comon_divider;
+
+		this->set_integer(integer);
+		this->set_numerator(numerator);
+		this->set_denominator(denominator);
+
+		return *this;
+	}
+
+	Fraction& operator/=(const Fraction& other)
+	{
+		int denominator = 1;
+		int numerator = 0;
+		int integer = 0;
+
+		int numerator_left = 0;
+		int numerator_right = 0;
+		if (this->get_integer() >= 0) numerator_left = (this->get_integer() * this->get_denominator() + this->get_numerator());
+		else numerator_left = (this->get_integer() * this->get_denominator() - this->get_numerator());
+		if (other.get_integer() >= 0) numerator_right = (other.get_integer() * other.get_denominator() + other.get_numerator());
+		else numerator_right = (other.get_integer() * other.get_denominator() - other.get_numerator());
+
+		int denominator_left = this->get_denominator();
+		int denominator_right = other.get_denominator();
+
+		numerator = numerator_left * denominator_right;
+		denominator = denominator_left * numerator_right;
+		integer = numerator / denominator;
+		numerator %= denominator;
+		if (numerator < 0) numerator *= (-1);
+
+		int max_comon_divider = 1;
+		for (int i = 1; i <= numerator; i++)
+		{
+			if (numerator % i == 0 && denominator % i == 0) max_comon_divider = i;
+		}
+
+		numerator /= max_comon_divider;
+		denominator /= max_comon_divider;
+
+		this->set_integer(integer);
+		this->set_numerator(numerator);
+		this->set_denominator(denominator);
+
+		return *this;
+	}
+
 	// Methods
 	void print()const
 	{
@@ -92,12 +284,277 @@ public:
 	}
 };
 
+Fraction operator+(const Fraction& left, const Fraction& right)
+{
+	Fraction result;
+	int denominator = 1;
+	int numerator = 0;
+	int integer = 0;
 
+	int numerator_left = 0;
+	int numerator_right = 0;
+
+	if (left.get_integer() >= 0) numerator_left = (left.get_integer() * left.get_denominator() + left.get_numerator());
+	else numerator_left = (left.get_integer() * left.get_denominator() - left.get_numerator());
+	if (right.get_integer() >= 0) numerator_right = (right.get_integer() * right.get_denominator() + right.get_numerator());
+	else numerator_right = (right.get_integer() * right.get_denominator() - right.get_numerator());
+	denominator = left.get_denominator() * right.get_denominator();
+	numerator_left *= right.get_denominator();
+	numerator_right *= left.get_denominator();
+
+	numerator = numerator_left + numerator_right;
+	integer = numerator / denominator;
+	numerator %= denominator;
+	if (numerator < 0) numerator *= (-1);
+
+	int max_comon_divider = 1;
+	for (int i = 1; i <= numerator; i++)
+	{
+		if (numerator % i == 0 && denominator % i == 0) max_comon_divider = i;
+	}
+
+	numerator /= max_comon_divider;
+	denominator /= max_comon_divider;
+	
+	result.set_integer(integer);
+	result.set_numerator(numerator);
+	result.set_denominator(denominator);
+	
+	return result;
+}
+
+Fraction operator-(const Fraction& left, const Fraction& right)
+{
+	Fraction result;
+	int denominator = 1;
+	int numerator = 0;
+	int integer = 0;
+
+	int numerator_left = 0;
+	int numerator_right = 0;
+
+	if (left.get_integer() >= 0) numerator_left = (left.get_integer() * left.get_denominator() + left.get_numerator());
+	else numerator_left = (left.get_integer() * left.get_denominator() - left.get_numerator());
+	if (right.get_integer() >= 0) numerator_right = (right.get_integer() * right.get_denominator() + right.get_numerator());
+	else numerator_right = (right.get_integer() * right.get_denominator() - right.get_numerator());
+	denominator = left.get_denominator() * right.get_denominator();
+	numerator_left *= right.get_denominator();
+	numerator_right *= left.get_denominator();
+
+	numerator = numerator_left - numerator_right;
+	integer = numerator / denominator;
+	numerator %= denominator;
+	if (numerator < 0) numerator *= (-1);
+
+	int max_comon_divider = 1;
+	for (int i = 1; i <= numerator; i++)
+	{
+		if (numerator % i == 0 && denominator % i == 0) max_comon_divider = i;
+	}
+
+	numerator /= max_comon_divider;
+	denominator /= max_comon_divider;
+
+	result.set_integer(integer);
+	result.set_numerator(numerator);
+	result.set_denominator(denominator);
+
+	return result;
+}
+
+Fraction operator*(const Fraction& left, const Fraction& right)
+{
+	Fraction result;
+
+	int denominator = 1;
+	int numerator = 0;
+	int integer = 0;
+
+	int numerator_left = 0;
+	int numerator_right = 0;
+
+	if (left.get_integer() >= 0) numerator_left = (left.get_integer() * left.get_denominator() + left.get_numerator());
+	else numerator_left = (left.get_integer() * left.get_denominator() - left.get_numerator());
+	if (right.get_integer() >= 0) numerator_right = (right.get_integer() * right.get_denominator() + right.get_numerator());
+	else numerator_right = (right.get_integer() * right.get_denominator() - right.get_numerator());
+
+	numerator = numerator_left * numerator_right;
+	denominator = left.get_denominator() * right.get_denominator();
+	integer = numerator / denominator;
+	numerator %= denominator;
+	if (numerator < 0) numerator *= (-1);
+
+	int max_comon_divider = 1;
+	for (int i = 1; i <= numerator; i++)
+	{
+		if (numerator % i == 0 && denominator % i == 0) max_comon_divider = i;
+	}
+
+	numerator /= max_comon_divider;
+	denominator /= max_comon_divider;
+
+	result.set_integer(integer);
+	result.set_numerator(numerator);
+	result.set_denominator(denominator);
+
+	return result;
+}
+
+Fraction operator/(const Fraction& left, const Fraction& right)
+{
+	Fraction result;
+
+	int denominator = 1;
+	int numerator = 0;
+	int integer = 0;
+
+	int numerator_left = 0;
+	int numerator_right = 0;
+	if (left.get_integer() >= 0) numerator_left = (left.get_integer() * left.get_denominator() + left.get_numerator());
+	else numerator_left = (left.get_integer() * left.get_denominator() - left.get_numerator());
+	if (right.get_integer() >= 0) numerator_right = (right.get_integer() * right.get_denominator() + right.get_numerator());
+	else numerator_right = (right.get_integer() * right.get_denominator() - right.get_numerator());
+
+	int denominator_left = left.get_denominator();
+	int denominator_right = right.get_denominator();
+
+	numerator = numerator_left * denominator_right;
+	denominator = denominator_left * numerator_right;
+	integer = numerator / denominator;
+	numerator %= denominator;
+	if (numerator < 0) numerator *= (-1);
+
+	int max_comon_divider = 1;
+	for (int i = 1; i <= numerator; i++)
+	{
+		if (numerator % i == 0 && denominator % i == 0) max_comon_divider = i;
+	}
+
+	numerator /= max_comon_divider;
+	denominator /= max_comon_divider;
+
+	result.set_integer(integer);
+	result.set_numerator(numerator);
+	result.set_denominator(denominator);
+
+	return result;
+}
+
+bool operator==(const Fraction& left, const Fraction& right)
+{
+	return (left.get_integer() == right.get_integer() && left.get_numerator() == right.get_numerator() && left.get_denominator() == right.get_denominator());
+}
+
+bool operator!=(const Fraction& left, const Fraction& right)
+{
+	if ((left.get_integer() != right.get_integer() || left.get_numerator() != right.get_numerator() || left.get_denominator() != right.get_denominator()))
+	{
+		return true;
+	}
+	else return false;
+}
+
+bool operator>(const Fraction& left, const Fraction& right)
+{
+	int numerator_left = 0;
+	int numerator_right = 0;
+
+	if (left.get_integer() >= 0) numerator_left = (left.get_integer() * left.get_denominator() + left.get_numerator());
+	else numerator_left = (left.get_integer() * left.get_denominator() - left.get_numerator());
+	if (right.get_integer() >= 0) numerator_right = (right.get_integer() * right.get_denominator() + right.get_numerator());
+	else numerator_right = (right.get_integer() * right.get_denominator() - right.get_numerator());
+	
+	numerator_left *= right.get_denominator();
+	numerator_right *= left.get_denominator();
+
+	return (numerator_left > numerator_right);
+}
+
+bool operator<(const Fraction& left, const Fraction& right)
+{
+	int numerator_left = 0;
+	int numerator_right = 0;
+
+	if (left.get_integer() >= 0) numerator_left = (left.get_integer() * left.get_denominator() + left.get_numerator());
+	else numerator_left = (left.get_integer() * left.get_denominator() - left.get_numerator());
+	if (right.get_integer() >= 0) numerator_right = (right.get_integer() * right.get_denominator() + right.get_numerator());
+	else numerator_right = (right.get_integer() * right.get_denominator() - right.get_numerator());
+
+	numerator_left *= right.get_denominator();
+	numerator_right *= left.get_denominator();
+
+	return (numerator_left < numerator_right);
+}
+
+bool operator>=(const Fraction& left, const Fraction& right)
+{
+	int numerator_left = 0;
+	int numerator_right = 0;
+
+	if (left.get_integer() >= 0) numerator_left = (left.get_integer() * left.get_denominator() + left.get_numerator());
+	else numerator_left = (left.get_integer() * left.get_denominator() - left.get_numerator());
+	if (right.get_integer() >= 0) numerator_right = (right.get_integer() * right.get_denominator() + right.get_numerator());
+	else numerator_right = (right.get_integer() * right.get_denominator() - right.get_numerator());
+
+	numerator_left *= right.get_denominator();
+	numerator_right *= left.get_denominator();
+
+	return (numerator_left >= numerator_right);
+}
+
+bool operator<=(const Fraction& left, const Fraction& right)
+{
+	int numerator_left = 0;
+	int numerator_right = 0;
+
+	if (left.get_integer() >= 0) numerator_left = (left.get_integer() * left.get_denominator() + left.get_numerator());
+	else numerator_left = (left.get_integer() * left.get_denominator() - left.get_numerator());
+	if (right.get_integer() >= 0) numerator_right = (right.get_integer() * right.get_denominator() + right.get_numerator());
+	else numerator_right = (right.get_integer() * right.get_denominator() - right.get_numerator());
+
+	numerator_left *= right.get_denominator();
+	numerator_right *= left.get_denominator();
+
+	return (numerator_left <= numerator_right);
+}
+
+std::ostream& operator<<(std::ostream& os, const Fraction& obj)
+{
+	if (obj.get_integer() && obj.get_numerator())
+	{
+		return os << obj.get_integer() << "(" << obj.get_numerator() << "/" << obj.get_denominator() << ")" << endl;
+	}
+	else if (obj.get_integer() == 0 && obj.get_numerator())
+	{
+		return os << "(" << obj.get_numerator() << "/" << obj.get_denominator() << ")" << endl;
+	}
+	else if (obj.get_integer() == 0 && obj.get_numerator() == 0)
+	{
+		return os << 0 << endl;
+	}
+	cout << endl;
+}
+
+
+std::istream& operator>>(std::istream& is, Fraction& obj)
+{
+	int integer, numerator, denominator;
+	is >> integer >> numerator >> denominator;
+
+	obj.set_integer(integer);
+	obj.set_numerator(numerator);
+	obj.set_denominator(denominator);
+
+	return is;
+}
+
+//#define CHECK_CONSTRUCTORS
 
 void main()
 {
 	setlocale(LC_ALL, "");
 
+#if defined CHECK_CONSTRUCTORS
 	Fraction A; // Default constructor
 	A.print();
 
@@ -116,4 +573,56 @@ void main()
 	Fraction F;
 	F = E;          // Copy assignment
 	F.print(); 
-}
+#endif // CHECK_CONSTRUCTORS
+
+	// Сложение
+	Fraction A(-2, 3, 5);
+	Fraction B(-5, 6, 7);
+	Fraction C = A + B;
+	C.print();
+
+	// Вычитание
+	Fraction D(5, 7, 9);
+	Fraction E(3, 1, 3);
+	Fraction F = D - E;
+	F.print();
+
+	// Умножение
+	Fraction H(5, 3, 4);
+	Fraction I(7, 1, 7);
+	Fraction J = H * I;
+	J.print();
+
+	// Деление
+	Fraction K(7, 2, 3);
+	Fraction L(2, 1, 8);
+	Fraction M = K / L;
+	M.print();
+
+	// ++
+	++K;
+	K.print();
+	L++;
+	L.print();
+
+	// --
+	Fraction N(1, 3);
+	--N;
+	N.print();
+	N--;
+	N.print();
+
+	// +=
+	A += B;
+	A.print();
+
+	// ==
+	Fraction O(5, 3, 4);
+	cout << (O == H) << endl;
+
+	// cin
+	Fraction P;
+	cout << "Введите целое, числитель и знаминатель дробного числа: "; cin >> P;
+	cout << endl;
+	cout << P;
+} 
